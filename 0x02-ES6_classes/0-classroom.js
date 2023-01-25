ac2992860750b382beeb0731ddcc8b9ce08e0d7a
NatnaelFY <natfiky2020@gmail.com>
2023-01-25 23:08:11 +0300

    specialization2

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,18 +1,5 @@
-// async function that will call these two functions and return an object
-import { uploadPhoto, createUser } from './utils';
-
-export default async function asyncUploadUser() {
-  try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    return {
-      photo,
-      user,
-    };
-  } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
