9f3949b953119a04fe4e50e85a73836519873b41
Natnael Fikre <102257616+NatnaelFY@users.noreply.github.com>
2023-01-26 14:14:35 +0300

    specialization1

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const a = [];
-    for (const idx of array) {
-      a.push(appendString + idx);
-    }
-  
-    return a;
+  const a = [];
+  for (const idx of array) {
+    a.push(appendString + idx);
   }
-  
+  return a;
+}
