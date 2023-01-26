3ed2f6a593d0e5c6afc9fb139fdfa7fe076c2654
Natnael Fikre <102257616+NatnaelFY@users.noreply.github.com>
2023-01-26 14:15:18 +0300

    specialization1

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,12 @@
 export default function createIteratorObject(report) {
-    return {
-      * [Symbol.iterator]() {
-        for (const department of Object.values(report.allEmployees)) {
-          for (const employee of department) {
-            yield employee;
-          }
+  return {
+    * [Symbol.iterator]() {
+      for (const department of Object.values(report.allEmployees)) {
+        for (const employee of department) {
+          yield employee;
         }
-      },
-    };
-  }
+      }
+    },
+  };
+}
   
